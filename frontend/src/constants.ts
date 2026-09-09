091a92c5d0e54ebb293fdd9a933ae832a99d8e9e
Aidana D <dusekeevaajdana5@gmail.com>
2026-09-09 22:29:04 +0700

    Started doing HomeWork #91. Installed dependencies. Created an account in Google Cloud and added it in constants.ts and in main.tsx

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,1 +1,3 @@
 export const BASE_URL = 'http://localhost:8088';
+export const GOOGLE_CLIENT_ID = '422735147692-jvjabadvng8c2ssk9gj9d1fcsau0g6mg.apps.googleusercontent.com';
+export const GOOGLE_SECRET = "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";
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,14 +5,18 @@ import {Provider} from "react-redux"
 import {persistor, store} from "./app/store.ts"
 import {CssBaseline} from "@mui/material"
 import {PersistGate} from "redux-persist/integration/react";
+import {GoogleOAuthProvider} from "@react-oauth/google";
+import {GOOGLE_CLIENT_ID} from "./constants.ts"
 
 createRoot(document.getElementById('root')!).render(
-    <Provider store={store}>
-        <PersistGate persistor={persistor}>
-            <BrowserRouter>
-                <CssBaseline/>
-                <App />
-            </BrowserRouter>
-        </PersistGate>
-    </Provider>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+        <Provider store={store}>
+            <PersistGate persistor={persistor}>
+                <BrowserRouter>
+                    <CssBaseline/>
+                    <App />
+                </BrowserRouter>
+            </PersistGate>
+        </Provider>
+    </GoogleOAuthProvider>
 )
